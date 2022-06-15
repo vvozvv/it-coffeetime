d30f385e9e28671369c11f3afa3e1e8cc12d6e0b
Алексей Важов <a.vazhov.cm@gmail.com>
2022-06-15 17:56:19 +0300

    init app

--- next.config.js
+++ next.config.js
@@ -1,39 +1,9 @@
 /** @type {import('next').NextConfig} */
-const withCss = require('next-css');
-const withTs = require('next-typescript');
-
-
-module.exports = withTs({
-  /* additional config*/
-});
-
-
-module.exports = withCss({
-  /* extra optional config */
-});
 
 const nextConfig = {
   reactStrictMode: true,
   webpack(config, options) {
     const { dev, isServer } = options;
-
-    // if (!dev) {
-    //   config.devtool = 'source-map';
-    // }
-
-    const extractCSSPlugin = new ExtractTextPlugin({
-      filename: 'static/style.css',
-      disable: dev,
-    });
-    config.module.rules.push({
-      test: /\\.css$/,
-      use: cssLoaderConfig(extractCSSPlugin, {
-        cssModules,
-        dev,
-        isServer,
-      }),
-    });
-    
     return config;
   },
 }
